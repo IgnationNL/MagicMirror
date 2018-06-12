c4c59ca555ab4148066be07439b47cc9522e1ed3
Annick van der Hoest <annick.vander.hoest@ignation.io>
2018-06-12 16:43:54 +0200

    Updated README and config file

---
+++ config.js
@@ -0,0 +1,80 @@
+/* Magic Mirror Config Sample
+ *
+ * By Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ *
+ * For more information how you can configurate this file
+ * See https://github.com/MichMich/MagicMirror#configuration
+ *
+ */
+
+var config = {
+	address: "localhost", // Address to listen on, can be:
+	                      // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+	                      // - another specific IPv4/6 to listen on a specific interface
+	                      // - "", "0.0.0.0", "::" to listen on any interface
+	                      // Default, when address config is left out, is "localhost"
+	port: 8080,
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
+	                                                       // or add a specific IPv4 of 192.168.1.5 :
+	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+	                                                       // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+
+	language: "en",
+	timeFormat: 24,
+	units: "metric",
+
+	modules: [
+		{
+			module: "alert",
+		},
+		{
+			module: 'MMM-KeyBindings',
+    			config: {
+        			enableNotifyServer: false,
+        			enableMousetrap: true,
+    			}
+		},
+		{
+			module: "clock",
+			position: "top_right",
+			config: {
+				timezone: "Europe/Amsterdam"
+			}
+		},
+		
+		{
+			module: "ignationfacerec",
+			position: "bottom_bar"
+		},
+{
+			module: "currentweather",
+			position: "top_left",
+			config: {
+				location: "Amsterdam",
+				locationID: "6544881",  //ID from http://www.openweathermap.org/help/city_list.txt
+				appid: "1b604c4e0a022785aa29433f8bb60c72"
+			}
+		},
+		{
+			module: "newsfeed",
+			position: "bottom_left",
+			config: {
+				feeds: [
+					{
+						title: "What's new?",
+						url: "https://www.ad.nl/home/rss.xml"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true
+			}
+		},
+		
+	]
+
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {module.exports = config;}
