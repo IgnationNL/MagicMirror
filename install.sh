458b3678a795c89523fb3c57456d7eed5b90e57d
Wesley <wesley.scheper@ignation.io>
2018-07-02 15:02:40 +0200

    Added install script.

---
+++ install.sh
@@ -0,0 +1,34 @@
+
+echo "######## Cloning Magic Mirror repository... ########"
+git clone https://github.com/MichMich/MagicMirror.git # Clone MagicMirror repo
+
+# Clone Ignation Magic Mirror repo. Note: SSH certificate needs to set up first.
+echo "######## Cloning Ignation repository... ########"
+git clone git@github.com:IgnationNL/MagicMirror.git IGMagicMirror
+# eof: Clone Ignation Magic Mirror repo
+
+# Copy files and modules from Ignation repo
+echo "######## Moving Ignation modules and files to MagicMirror... ########"
+cp IGMagicMirror/config.js MagicMirror/config/config.js # Copy config file
+cp IGMagicMirror/currentweather.css MagicMirror/modules/default/currentweather/currentweather.css
+cp IGMagicMirror/custom.css MagicMirror/css/custom.css
+cp -r IGMagicMirror/modules/* MagicMirror/modules/
+# eof: Copy files and modules from Ignation repo
+
+# Install dependencies
+echo "######## Installing Node.js depdencies... ########"
+cd MagicMirror/modules/ignationfacerec
+npm install
+cd ../ignationled
+npm install
+cd ../ignationlogodisplay
+npm install
+cd ../../
+npm install
+# eof: Install dependencies
+
+# Clean up
+echo "######## Cleaning up... ########"
+cd ../
+rm -r -f IGMagicMirror/
+# eof: Clean up
